1925702e20c603270c5288a679e0295373c083a3
vendetta935 <36058207+vendetta935@users.noreply.github.com>
2018-02-01 17:12:16 -0800

    Update theme-customisations.php

--- theme-customisations.php
+++ theme-customisations.php
@@ -115,3 +115,15 @@ function theme_customisations_main() {
  * Initialise the plugin
  */
 add_action( 'plugins_loaded', 'theme_customisations_main' );
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
